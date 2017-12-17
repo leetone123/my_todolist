eb3cbe09c436799fe26b3d397a72824c6667ea5b
leetone123 <leetone123@163.com>
2017-12-17 19:27:30 +0800

    解决发布部署后无法访问该页面

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
 
     // Paths
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: './static',
     assetsPublicPath: '/',
     proxyTable: {},
 
@@ -47,7 +47,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: './static',
     assetsPublicPath: '/',
 
     /**
