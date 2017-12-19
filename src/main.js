d48201f1201061f3da256afb051d49d140a5a27a
leetone123 <leetone123@163.com>
2017-12-19 22:06:10 +0800

    解决打包部署时css/js文件名不自动生成hash

--- src/main.js
+++ src/main.js
@@ -7,15 +7,28 @@ import axios from 'axios'
 Vue.prototype.$http = axios
 //使用第三方的类库对请求进行格式化
 import qs from 'qs';
-axios.interceptors.request.use( (config) => {
-    if (config.method=="post"){
-        config.data = qs.stringify(config.data);
-        config.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-    }
-    return config;
-},  (error) => {
-    return Promise.reject(error);
-});
+axios.defaults.headers.post['content-Type'] = 'appliction/x-www-form-urlencoded';
+let instance=axios.create({
+    //这里配置你自己的url
+    baseURL: 'http://localhost:9999/',
+    timeout: 10000,
+    //请求头
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    transformRequest: [
+        function (data) { 
+        data = qs.stringify(data)
+        return data }
+    ] 
+    })
+// axios.interceptors.request.use( (config) => {
+//     if (config.method=="post"){
+//         config.data = qs.stringify(config.data);
+//         config.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+//     }
+//     return config;
+// },  (error) => {
+//     return Promise.reject(error);
+// });
 
 
 Vue.config.productionTip = false
