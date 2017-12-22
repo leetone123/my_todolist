0121fc726d1490b8487f754195497620c74a916f
leetone123 <leetone123@163.com>
2017-12-22 16:23:22 +0800

    配置正式访问url

--- utils/date.js
+++ utils/date.js
@@ -26,5 +26,5 @@ function padLeftZero(str) {
     return ('00' + str).substr(str.length);
 }
 //本地测试时置为空，生成环境打包时设为本地url
-export const baseurl ="http://101.132.160.79:9999";
+export const baseurl ="http://localhost:9999";
 // export const baseurl ="";
